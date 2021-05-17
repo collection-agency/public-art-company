cd1b490d1638468e63041d086d7b843f5f5f195f
mikeyrayvon <mikey@interglobal.vision>
2021-05-17 11:26:23 +0200

    add project list

--- web/components/landing/ProjectList.js
+++ web/components/landing/ProjectList.js
@@ -6,41 +6,42 @@ const ProjectList = ({ docs, setSectionHeadline, headline }) => {
   let wasThirds = true
   let k = 0
 
+  const isThirds = () => {
+    if (wasThirds) {
+      if (k > 1) {
+        k = 0
+        wasThirds = false
+      } else {
+        k++
+      }
+      return true
+    } else {
+      if (k > 0) {
+        k = 0
+        wasThirds = true
+      } else {
+        k++
+      }
+      return false
+    }
+  }
+
   return (
     <div className='w-container mx-auto'>
       <div className='flex flex-wrap -mx-4'>
         {docs.map((doc, index) => {
           const { _key, title, slug, mainImage } = doc
-
-          let isThirds
-
-          if (wasThirds) {
-            if (k > 1) {
-              k = 0
-              wasThirds = false
-            } else {
-              k++
-            }
-            isThirds = true
-          } else {
-            if (k > 0) {
-              k = 0
-              wasThirds = true
-            } else {
-              k++
-            }
-            isThirds = false
-          }
-
           return (
             <div
               key={_key}
-              className={`px-4 w-full mb-8 ${(isThirds ? 'md:w-1/3' : 'md:w-1/2')}`}
-              onMouseEnter={() => setSectionHeadline(title)}
-              onMouseLeave={() => setSectionHeadline(headline)}>
-              <div className='h-72 bg-cover bg-center' style={{
-                backgroundImage: `url(${imageUrl(mainImage, 700, 500)})`
-              }}></div>
+              className={`px-4 w-full mb-8 ${(isThirds() ? 'md:w-1/3' : 'md:w-1/2')}`}>
+              <div
+                className='cursor-pointer h-72 bg-cover bg-center'
+                style={{
+                  backgroundImage: `url(${imageUrl(mainImage, 700, 500)})`
+                }}
+                onMouseEnter={() => setSectionHeadline(title)}
+                onMouseLeave={() => setSectionHeadline(headline)}></div>
             </div>
           )
         })}
