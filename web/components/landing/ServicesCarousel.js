d2d5827dffecf7314b9d0d34f6252077fceeb71f
mikeyrayvon <mikey@interglobal.vision>
2021-05-24 23:10:25 +0200

    improve servies carousel navigation

--- web/components/landing/ServicesCarousel.js
+++ web/components/landing/ServicesCarousel.js
@@ -3,7 +3,7 @@ import { imageUrl } from 'utils/helpers'
 import { PortableText } from 'utils/sanity'
 import SwiperCore, { Controller, Navigation, Pagination, EffectFade } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import CarouselNavigation from 'components/CarouselNavigation'
+import { CgArrowLeft, CgArrowRight } from 'react-icons/cg'
 
 SwiperCore.use([Controller, Navigation, Pagination, EffectFade]);
 
@@ -20,7 +20,7 @@ const ServicesCarousel = ({ items }) => {
 
   return (
     <div className='min-w-0 flex flex-wrap md:flex-nowrap px-8'>
-      <div className='w-full md:w-1/3 xl:w-1/2'>
+      <div className='w-full md:w-1/3 xl:w-2/5'>
         <Swiper
           onSwiper={setControlledSwiper}
           className='h-full'
@@ -32,14 +32,20 @@ const ServicesCarousel = ({ items }) => {
             return (
               <SwiperSlide key={`${_key}_b`} className='swiper-no-swiping pb-12 md:pb-0 md:pt-20 lg:pt-24 xl:pt-28 2xl:pt-36 bg-cream self-stretch min-h-0'>
                 {text &&
-                  <PortableText blocks={text} className='mx-auto w-full xl:w-2/3 2xl:w-1/2' renderContainerOnSingleChild />
+                  <PortableText blocks={text} className='mx-auto w-full xl:w-4/5 2xl:w-2/3' renderContainerOnSingleChild />
                 }
               </SwiperSlide>
             )
           })}
         </Swiper>
       </div>
-      <div className='h-96 md:h-400 xl:h-500 2xl:h-700 w-full md:w-2/3 xl:w-1/2'>
+
+      <div className='h-96 md:h-400 xl:h-500 2xl:h-700 w-full md:w-2/3 xl:w-3/5 flex flex-initial transform md:translate-x-16 2xl:translate-x-24'>
+        <div className='w-16 2xl:w-24 flex flex-initial justify-center items-center pointer-events-none'>
+          <div className='swiper-button-prev w-full z-10 pointer-events-auto text-center'>
+            <CgArrowLeft className='mx-auto w-3/5 2xl:w-1/2 h-full text-dark-gray hover:text-black' />
+          </div>
+        </div>
         <Swiper
           controller={{ control: controlledSwiper }}
           navigation={{
@@ -47,22 +53,25 @@ const ServicesCarousel = ({ items }) => {
             prevEl: '.swiper-button-prev'
           }}
           pagination={{ clickable: true }}
-          className='h-full relative md:transform md:translate-x-12'
+          className='flex-1 h-full relative'
           {...params}
         >
-          <CarouselNavigation />
           {items.map(item => {
             const { _key, image } = item
             return (
-              <SwiperSlide key={`${_key}_a`} className='md:px-12'>
+              <SwiperSlide key={`${_key}_a`} className=''>
                 {image &&
                   <div className='h-full bg-cover bg-center' style={{ backgroundImage: `url(${imageUrl(image, 960, 960)})` }} role='img' aria-label={image && image.alt}></div>
                 }
               </SwiperSlide>
             )
           })}
-          <CarouselNavigation />
         </Swiper>
+        <div className='w-16 2xl:w-24 flex-initial flex justify-center items-center pointer-events-none'>
+          <div className='swiper-button-next w-full z-10 pointer-events-auto'>
+            <CgArrowRight className='mx-auto w-3/5 2xl:w-1/2 h-full text-dark-gray hover:text-black' />
+          </div>
+        </div>
       </div>
     </div>
   )
