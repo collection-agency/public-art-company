f2ea2aebcc4279d6ae3b2f4c093a7650dee90e82
mikeyrayvon <mikey@interglobal.vision>
2021-06-28 14:22:22 +0200

    fix about text size for mobile

--- web/components/landing/About.js
+++ web/components/landing/About.js
@@ -12,7 +12,7 @@ const About = ({ data }) => {
       <div className='w-container mx-auto'>
         <div className='py-20 lg:py-24 xl:py-28 2xl:py-36'>
           {text &&
-            <PortableText blocks={text} className='text-center w-5/6 md:w-3/4 lg:w-2/3 xl:w-1/2 mx-auto text-4xl' renderContainerOnSingleChild />
+            <PortableText blocks={text} className='text-center w-5/6 md:w-3/4 lg:w-2/3 xl:w-1/2 mx-auto text-xl md:text-2xl  lg:text-3xl xl:text-4xl' renderContainerOnSingleChild />
           }
         </div>
       </div>
